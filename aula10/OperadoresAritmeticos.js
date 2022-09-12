e19dc547d1dcbbb0143da18f2765a569145da99e
Gabriel Chagas <gamesgchagas@gmail.com>
2022-09-12 12:27:27 -0300

    Atualização novas aulas

---
+++ aula10/OperadoresAritmeticos.js
@@ -0,0 +1,33 @@
+/* 
+Aritmeticos
+* + Adição / Concatenação
+* - Substração
+* / Divisão
+* * Multiplicação
+* ** Potenciação
+* % Resto da divisão
+* ++ Incremento
+* -- Decremento
+*/
+
+/*
+Precedência
+() calculo dentro do parenteses
+** Potenciação
+* - % Multiplicação, Divisão, Resto da divisão
++ - Adição, Subtração
+*/
+
+/*
+NaN  Not a Number
+Normalmente exibido quando se mistura numero e outro valor de tipo diferente
+A tratativa é utilizar a função parse'tipo' para realizar a conversão do valor
+Ex: parseInt (inteiro), parseFloat(decimais)
+*/
+
+let contador = 0;
+contador += 2;
+contador += 2;
+contador += 2;
+contador += 2;
+console.log(contador);
