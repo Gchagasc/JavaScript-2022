d345b2d3fbac14f3ceebdfc1602adda4afa670a1
Gabriel Chagas <gamesgchagas@gmail.com>
2022-09-26 03:04:55 -0300

    Atualização aulas

---
+++ aula31/letConst.js
@@ -0,0 +1,35 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco {... bloco }
+// Var só tem escopo de função
+
+/*
+let nome = 'Luiz'; // criando
+var nome2 = 'Luiz';
+
+if (verdadeira) {
+    let nome = 'Gabriel' // criando
+    //console.log(nome, nome2);
+
+    if (verdadeira) {
+        var nome2 = 'Gabriel'; //Redeclarada
+        let nome = 'Outra coisa'
+        console.log(nome, nome2);
+    }
+}
+*/
+
+/*
+var sobrenome = 'Chagas';
+
+function falaOi () {
+    var nome = 'Gabriel'
+    console.log(nome, sobrenome);
+}
+
+falaOi();
+*/
+
+console.log(sobrenome);
+
+var sobrenome ='Chagas';
